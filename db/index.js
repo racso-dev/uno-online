6fa699d21f4b903ab6d6cd198bbda90134f549c2
Oscar Rénier <oscar.renier@gmail.com>
2022-03-14 04:31:22 -0700

    [FIX]: SSL enforced for db config

--- db/index.js
+++ db/index.js
@@ -2,6 +2,9 @@ const pgp = require("pg-promise")();
 if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
 }
-const connection = pgp(process.env.DATABASE_URL);
+const connection = pgp({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
 module.exports = connection;
