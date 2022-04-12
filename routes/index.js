c5913724ed3cee8893aa252cd8016510e2feaad1
Nicolas Antoine <nicolas.antauf@gmail.com>
2022-04-11 18:16:43 -0700

    [ADD]: Switched templating libraries and lobby page scaffold

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('landing', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
 router.get('/lobby', function(req, res, next) {
   res.render('lobby', { title: 'Express' });
 });
 
-router.get('/signup', function(req, res, next) {
-  res.render('signup', { title: 'Express' });
-});
+// router.get('/signup', function(req, res, next) {
+//   res.render('signup', { title: 'Express' });
+// });
 
-router.get('/signin', function(req, res, next) {
-  res.render('signin', { title: 'Express' });
-});
+// router.get('/signin', function(req, res, next) {
+//  res.render('signin', { title: 'Express' });
+// });
 
 module.exports = router;
