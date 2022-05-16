cb30b4288e915d1103251abaeff02c6849df39f4
al20ov <nicolas.antauf@gmail.com>
2022-05-15 20:00:47 -0700

    [ADD]: base dom selector logic in lobby

---
+++ public/scripts/lobby.js
@@ -0,0 +1,14 @@
+// Deck filling order besides player deck since the player's deck will always be
+// the first and at the bottom
+
+const fillOrder = [
+    'top-deck',
+    'left-deck',
+    'right-deck',
+];
+
+const playerDeck = document.getElementById('player-deck');
+const topDeck = document.getElementById('top-deck');
+const leftDeck = document.getElementById('left-deck');
+const rightDeck = document.getElementById('right-deck');
+
